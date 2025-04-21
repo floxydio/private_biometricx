a5deda464985663af1980ecc5d32f5838b017abc
Dio Rovelino <63448336+floxydio@users.noreply.github.com>
2025-04-21 13:05:50 +0700

    biometric commit update crypto

---
+++ integration-tests/testapp/src/androidTest/java/androidx/biometric/integration/testapp/TestUtils.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.biometric.integration.testapp
+
+import android.app.KeyguardManager
+import android.content.Context
+import android.os.Build
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators
+import androidx.biometric.BiometricManager.BIOMETRIC_SUCCESS
+
+/** Checks [context] to determine if the device has an enrolled biometric authentication method. */
+internal fun hasEnrolledBiometric(context: Context): Boolean {
+    val biometricManager = BiometricManager.from(context)
+    return biometricManager.canAuthenticate(Authenticators.BIOMETRIC_WEAK) == BIOMETRIC_SUCCESS
+}
+
+/** Checks [context] to determine if the device is currently locked. */
+internal fun isDeviceLocked(context: Context): Boolean {
+    val keyguard =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            context.getSystemService(KeyguardManager::class.java)
+        else context.getSystemService(Context::KEYGUARD_SERVICE.toString()) as KeyguardManager?
+
+    return when {
+        keyguard == null -> false
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1 -> keyguard.isDeviceLocked
+        else -> keyguard.isKeyguardLocked
+    }
+}
